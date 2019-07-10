5236db89230f972ce6337f1700bda1b9aa3f406b
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-07-09 19:30:20 -0500

    Added header component with navigation

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
 
 function App() {
     return (
         <div className="container-fluid">
+            {/* Calling header about Routes will always display the heading */}
+            <Header />
+
             {/* Setting up app routing */}
             <Route exact path="/" component={HomePage} />
             <Route path="/about" component={AboutPage} />
